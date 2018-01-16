949400b244257e27fe8128ecdc69d9ae5cee1968
Michael John Decker <mdecker6@kent.edu>
2018-01-16 16:43:18 -0500

    Made main.cpp less empty

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ *
+ * This is a generic C++ main file.
+ *
+ * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ */
+
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+    std::cout << "Hello World!!!\n";
+
+    return 0;
+}
