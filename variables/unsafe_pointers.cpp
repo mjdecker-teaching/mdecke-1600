30e0af4048685d1ea8d320def979e280beb979cd
Michael John Decker <mdecker6@kent.edu>
2018-01-25 16:25:33 -0500

    Add unsafe pointers example

---
+++ variables/unsafe_pointers.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ *
+ * Unsafe pointer examples.
+ *
+ * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char * argv[]) {
+
+    int integers[5] = { 0x65666768, 92, 4, 5, 0x2A };
+    for(int integer : integers) {
+        std::cout << integer << ' ';
+    }
+    std::cout << '\n';
+
+    char * characters = (char *)integers;
+    for(int i = 0; i < 5; ++i) {
+        std::cout << characters[i] << ' ';
+    }
+    std::cout << '\n';
+
+    std::string * str = (std::string *)integers;
+    std::cout << str->size() << '\n';
+    std::cout << '\'' << str->c_str() << "'\n";
+
+
+    return 0;
+}
