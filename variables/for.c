daf33b0cfc05081ba96705d621184434b7461b9e
Michael John Decker <mdecker6@kent.edu>
2018-01-25 17:31:52 -0500

    Add for (C) example

---
+++ variables/for.c
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ *
+ * for-declaratio example
+ *
+ * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ */
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+
+  int i;
+  for(i = 0; i < 10; ++i) {
+    printf("%d ", i);
+  }
+  printf("\n");
+
+  return 0;
+}
