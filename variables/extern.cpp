0fe7446acc59b4b86ef17f71dd253f018c707994
Michael John Decker <mdecker6@kent.edu>
2018-01-27 10:55:07 -0500

    Add extern example

---
+++ variables/extern.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ *
+ * This is a generic C++ main file.
+ *
+ * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ */
+
+#include <iostream>
+
+extern int foo;
+
+int main(int argc, char * argv[]) {
+
+    std::cout << foo << '\n';
+
+    return 0;
+}
